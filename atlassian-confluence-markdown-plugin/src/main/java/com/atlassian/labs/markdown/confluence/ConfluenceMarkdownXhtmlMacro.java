8201b24834fd42f00ebbd64dce004b38fedd96f3
bbaker <devnull@localhost>
2012-03-25 21:17:54 +1100

    got i18n resources working correctly

--- atlassian-confluence-markdown-plugin/src/main/java/com/atlassian/labs/markdown/confluence/ConfluenceMarkdownXhtmlMacro.java
+++ atlassian-confluence-markdown-plugin/src/main/java/com/atlassian/labs/markdown/confluence/ConfluenceMarkdownXhtmlMacro.java
@@ -1,10 +1,11 @@
 package com.atlassian.labs.markdown.confluence;
 
 import com.atlassian.confluence.content.render.xhtml.ConversionContext;
+import com.atlassian.confluence.content.render.xhtml.macro.annotation.Format;
+import com.atlassian.confluence.content.render.xhtml.macro.annotation.RequiresFormat;
 import com.atlassian.confluence.macro.Macro;
 import com.atlassian.confluence.macro.MacroExecutionException;
-import com.atlassian.confluence.util.GeneralUtil;
-import com.atlassian.confluence.xhtml.api.XhtmlContent;
+import com.atlassian.renderer.v2.macro.MacroException;
 
 import java.util.Map;
 
@@ -13,26 +14,32 @@ import java.util.Map;
  */
 public class ConfluenceMarkdownXhtmlMacro implements Macro
 {
-    private final XhtmlContent xhtmlUtils;
 
-    public ConfluenceMarkdownXhtmlMacro(final XhtmlContent xhtmlUtils)
+    private final ConfluenceMarkdownMacro oldMarkdownMacro;
+
+    public ConfluenceMarkdownXhtmlMacro()
     {
-        this.xhtmlUtils = xhtmlUtils;
+        oldMarkdownMacro = new ConfluenceMarkdownMacro();
     }
 
     @Override
+    @RequiresFormat(Format.Storage)
     public String execute(Map<String, String> parameters, String body, ConversionContext context) throws MacroExecutionException
     {
-        return "<div>" +
-                "<h2>This would be markdown generated</h2>" +
-                "<pre><code>" + GeneralUtil.escapeXml(body) + "</code></pre>" +
-                "</div>";
+        try
+        {
+            return oldMarkdownMacro.execute(parameters, body, context != null ? context.getPageContext() : null);
+        }
+        catch (MacroException e)
+        {
+            throw new MacroExecutionException(e);
+        }
     }
 
     @Override
     public BodyType getBodyType()
     {
-        return BodyType.RICH_TEXT;
+        return BodyType.PLAIN_TEXT;
     }
 
     @Override
